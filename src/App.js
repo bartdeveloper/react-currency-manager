d386858e03cd7ca3d8cc6d93f4e7d2b23f27e039
Bartek <bartek@appselio.pl>
2019-01-11 14:48:08 +0100

    Lazy loading for Currency component.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, lazy, Suspense } from 'react';
 import './App.css';
 import Start from './components/Start';
 import Contact from './components/Contact';
 import NotFound from './components/NotFound';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import styled from 'styled-components'
-import Currency from './components/Currency'
 import Header from './containers/Header'
 import Footer from './containers/Footer';
 import { Link } from 'react-router-dom'
+
+const Currency = lazy( () => import('./components/Currency'))
 
 const Container = styled.div`
     background: #c0c0c0;
@@ -42,24 +43,26 @@ class App extends Component {
     return (
 
       <Router>
-        <Container>
-        
-          <Header txt="Currency manager" />
-            
-          <Switch>
-            <Route exact path='/' component={Start} />
-            <Route exact path='/contact' component={Contact} />
-            <Route exact path='/currency/:name' component={Currency} />
-            <Route component={NotFound} />
-          </Switch>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Container>
           
-          <SmallContainer>
-            <StyledLink to='/Contact'>Contact</StyledLink>
-          </SmallContainer>
+            <Header txt="Currency manager" />
+              
+            <Switch>
+              <Route exact path='/' component={Start} />
+              <Route exact path='/contact' component={Contact} />
+              <Route exact path='/currency/:name' component={Currency} />
+              <Route component={NotFound} />
+            </Switch>
+            
+            <SmallContainer>
+              <StyledLink to='/Contact'>Contact</StyledLink>
+            </SmallContainer>
 
-          <Footer />
+            <Footer />
 
-        </Container>
+          </Container>
+        </Suspense>
       </Router>
 
     );
