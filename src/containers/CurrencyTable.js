1c5ac2b62748e721a6031a17a2113d5a85e64d2f
Bartek <bartek@appselio.pl>
2019-01-17 22:22:48 +0100

    Search for currencies.

--- src/containers/CurrencyTable.js
+++ src/containers/CurrencyTable.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react'
-import CurrencyItem from './CurrencyItem'
+import CurrencyItem from './CurrencyTableItem'
 
 class CurrencyTable extends Component {
 
+    state = {
+        rates: this.props.rates
+    }
+
+    filterCurrencies(e) {
+        const text = e.currentTarget.value;
+        const filteredRates = this.getFilteredCurrenciesForText(text)
+        this.setState({
+          rates: filteredRates
+        });
+    }
+
+    getFilteredCurrenciesForText(text) {
+        var list = this.props.rates.filter(c => c.code.toLowerCase().includes(text.toLowerCase()))
+
+        if(list.length > 0) return list
+        else return this.props.rates
+    }
+
   render() {
 
-    const { table, no, date, rates } = this.props
+    const { table, no, date } = this.props
 
     return(
         <div>
             <div className="currency-header">
                 <strong>Table {table}</strong> no <strong>{no}</strong> from <strong>{date}</strong>
             </div>
+            <div className="currency-input">
+                <input onInput={this.filterCurrencies.bind(this)} placeholder="Type currency" />
+            </div>
             <div className="currency-table">
                 <div className="currency-table-items">
-                    {rates.map(r =>
+                    {this.state.rates.map(r =>
                         <CurrencyItem
                             key={r.code}
                             code={r.code}
@@ -26,6 +48,7 @@ class CurrencyTable extends Component {
             </div>
         </div>
     )
+    
   }
 }
 
