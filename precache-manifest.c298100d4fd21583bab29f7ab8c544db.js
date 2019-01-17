d932ca0c86f3f6db038821be8aec68b9309d6f33
Bartek <bartek@appselio.pl>
2019-01-17 23:33:17 +0100

    Updates

---
+++ precache-manifest.c298100d4fd21583bab29f7ab8c544db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d8cee259bf33a1a1898d",
+    "url": "/currency-manager/static/js/runtime~main.d8cee259.js"
+  },
+  {
+    "revision": "f01d1f9c5a529625129d",
+    "url": "/currency-manager/static/js/main.f01d1f9c.chunk.js"
+  },
+  {
+    "revision": "3c26e33d849237e2e80e",
+    "url": "/currency-manager/static/js/3.3c26e33d.chunk.js"
+  },
+  {
+    "revision": "d50d2c8f0428a620a3d3",
+    "url": "/currency-manager/static/js/2.d50d2c8f.chunk.js"
+  },
+  {
+    "revision": "bf63d24d561f15316b7e",
+    "url": "/currency-manager/static/js/1.bf63d24d.chunk.js"
+  },
+  {
+    "revision": "f01d1f9c5a529625129d",
+    "url": "/currency-manager/static/css/main.91381ce8.chunk.css"
+  },
+  {
+    "revision": "3c26e33d849237e2e80e",
+    "url": "/currency-manager/static/css/3.e117470b.chunk.css"
+  },
+  {
+    "revision": "21d19d7d872dbaa8429a1fce5ea4cec4",
+    "url": "/currency-manager/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/currency-manager/precache-manifest.c298100d4fd21583bab29f7ab8c544db.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/currency-manager/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
